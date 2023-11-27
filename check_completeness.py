93b625a08dec95f0da7efe214a103ca3ce862c1e
jesajx <johannes.olegard@olissoft.com>
2023-11-27 06:50:34 +0100

    check_completeness.py progress per venue

--- check_completeness.py
+++ check_completeness.py
@@ -2,6 +2,7 @@
 
 import json
 import re
+import pathlib
 
 with open("tags.json") as file:
     tags_from_doi = json.load(file)
@@ -9,6 +10,12 @@ with open("tags.json") as file:
 with open("all_dois.txt") as file:
     dois = set(file.read().splitlines())
 
+dois_by_venue = dict()
+for path in pathlib.Path(".").glob("dois/*.txt"):
+    venue = path.stem
+    with path.open() as file:
+        dois_by_venue[venue] = set(file.read().splitlines())
+        dois_by_venue[venue] -= {""} # remove empty line
 
 for doi, tags in tags_from_doi.items():
     new_tags = set()
@@ -39,4 +46,13 @@ num_done = len(done)
 num_left = num_tot - num_done
 done_percentage = num_done / num_tot * 100
 
-print(f"{num_done}/{num_tot} ({done_percentage:.2f}%) num_left={num_left}")
+print(f'{num_done}/{num_tot} ({done_percentage:.2f}%) num_left={num_left}')
+
+for venue in dois_by_venue:
+    venue_num_tot = len(dois_by_venue[venue])
+    venue_num_done = sum(1 for doi in dois_by_venue[venue] if doi in done)
+    venue_num_left = venue_num_tot - venue_num_done
+    venue_done_percentage = venue_num_done / venue_num_tot * 100
+    print(f'{venue}:')
+    print(f'  {venue_num_done}/{venue_num_tot} ({venue_done_percentage:.2f}%) num_left={venue_num_left}')
+
